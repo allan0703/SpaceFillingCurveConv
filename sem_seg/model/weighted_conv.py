405e7a60deb840114977c8d9b67f81e9157f88a1
Ali Thabet <alitabet@gmail.com>
2020-01-29 16:37:54 +0300

    Implement weighted transpose convolution

--- sem_seg/model/weighted_conv.py
+++ sem_seg/model/weighted_conv.py
@@ -38,6 +38,46 @@ class WeightedConv(nn.Module):
         return out
 
 
+class WeightedConvTranspose(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation, padding, stride):
+        super(WeightedConvTranspose, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.kernel_size = kernel_size
+        self.dilation = dilation
+        self.padding = padding
+        self.stride = stride
+
+        self.weight = nn.Parameter(torch.Tensor(self.out_channels, self.in_channels, self.kernel_size[0]))
+
+    def forward(self, x, coords, sigma):
+        x_t = torch.zeros((x.shape[0], x.shape[1], self.stride[0] * x.shape[2], self.stride[1] * x.shape[3]),
+                          dtype=x.dtype)
+        x_t[:, :, ::self.stride[0], ::self.stride[1]] = x
+
+        coords_t = torch.zeros((coords.shape[0], coords.shape[1], self.stride[0] * coords.shape[2],
+                                self.stride[1] * coords.shape[3]), dtype=coords.dtype)
+        coords_t[:, :, ::self.stride[0], ::self.stride[1]] = coords
+
+        windows = F.unfold(x_t, kernel_size=self.kernel_size, padding=self.padding,
+                           dilation=self.dilation, stride=1)
+        dist_weights = F.unfold(coords_t, kernel_size=self.kernel_size, padding=self.padding,
+                                dilation=self.dilation, stride=1)
+
+        dist_weights = dist_weights.view(x.shape[0], 3, self.kernel_size[0], -1)
+        dist_weights = dist_weights - dist_weights[:, :, self.kernel_size[0] // 2, :].unsqueeze(-2)
+        dist_weights = 1 - torch.sqrt(torch.sum(dist_weights ** 2, dim=1)) / sigma
+        dist_weights[dist_weights < 0] = 0.0
+        dist_weights = dist_weights.repeat(1, self.in_channels, 1)
+
+        windows = windows * dist_weights
+
+        out = windows.transpose(1, 2).matmul(self.weight.view(self.weight.size(0), -1).t()).transpose(1, 2)
+
+        return out
+
+
 class SeparableWeightedConv(nn.Module):
     def __init__(self, in_channels, kernel_size, dilation, padding, stride):
         super(SeparableWeightedConv, self).__init__()
@@ -82,6 +122,19 @@ class WeightedConv1D(WeightedConv):
         return out.squeeze(-1)
 
 
+class WeightedConvTranspose1D(WeightedConvTranspose):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, padding=0, stride=1):
+        super(WeightedConvTranspose1D, self).__init__(in_channels, out_channels, (kernel_size, 1),
+                                                      (dilation, 1), (padding, 0), (stride, 1))
+
+    def forward(self, x, coords=None, sigma=0.08):
+        if coords is not None:
+            coords = coords.unsqueeze(-1)
+        out = super().forward(x.unsqueeze(-1), coords, sigma)
+
+        return out.squeeze(-1)
+
+
 class SeparableWeightedConv1D(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, dilation=1,
                  padding=0, stride=1, norm_layer=nn.BatchNorm1d):
@@ -102,24 +155,27 @@ if __name__ == '__main__':
     batch_size = 2
     in_channels = 4
     out_channels = 16
-    num_points = 4096
+    num_points = 1024
     kernel_size = 9
     padding = 4
-    dilation = 1
-    stride = 1
+    dilation = 2
+    stride = 2
 
     feats = torch.rand((batch_size, in_channels, num_points), dtype=torch.float)
     coords = torch.rand((batch_size, 3, num_points), dtype=torch.float)
     # idx = torch.randint(0, num_points, (batch_size, groups, num_points), dtype=torch.int64)
     # inverse_idx = torch.randint(0, num_points, (batch_size, groups, num_points), dtype=torch.int64)
 
+    conv = WeightedConvTranspose1D(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                   dilation=dilation, padding=padding, stride=stride)
+
     # conv = WeightedConv1D(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
     #                       dilation=dilation, padding=padding, stride=stride)
 
     # feats = torch.rand((batch_size, groups, in_channels, num_points), dtype=torch.float)
     # coords = torch.rand((batch_size, groups, 3, num_points), dtype=torch.float)
-    conv = SeparableWeightedConv1D(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
-                                   dilation=dilation, padding=padding, stride=stride)
+    # conv = SeparableWeightedConv1D(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+    #                                dilation=dilation, padding=padding, stride=stride)
 
     start_time = time.time()
     out = conv(feats, coords, sigma=1.5)
